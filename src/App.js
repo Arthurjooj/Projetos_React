7a2c156e34db6f5fadef882836976112a82d685b
Commite feito por Arthurjooj <arthurruthes2018@gmail.com>
2023-05-15 09:08:20 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import react from "react";
+import './App.css';
+
+export default function app(){
+    return <h1> Programando em react js</h1>;
+  
+}
+
+/*
+  function App(){
+    return <h1>PROGRAMANDO EM REACT JS<h1>
+
+  }
+
+  export default app;
+*/
